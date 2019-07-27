eb5ba90c8468db15ff1746f37b6b655e01f76367
Tran Quoc Khang <52618191+tranquockhangctu@users.noreply.github.com>
2019-07-27 13:21:44 +0700

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+// CONFIG
+
+
+
+
+
+//FUNCTION : DONT CHANGE ?
+
+$('.gioi-thieu').click(() => {
+    scrollToId('carplus_about');
+});
+
+$('.tinh-nang').click(() => {
+    scrollToId('carplus_tinhnang');
+});
+
+$('.dich-vu').click(() => {
+    scrollToId('carplus_dichvu');
+});
+
+$('.doi-tac').click(() => {
+    scrollToId('carplus_doitac');
+});
+
+$('.tai-xuong-app').click(() => {
+    scrollToId('carplus_download');
+});
+
+$('.carousel').carousel();
+
+function scrollToId(id) {
+    $('html, body').animate({
+        scrollTop: $(`#${id}`).offset().top
+    }, 1000)
+}
